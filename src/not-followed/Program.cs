6ad8b0167c08154a5bf9fa2e1bdcfe3567b69ccc
Vedant Phougat <vedant.phougat@bdgloballogic.onmicrosoft.com>
2020-04-03 19:46:47 +0530

    Add project files.

---
+++ src/not-followed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs.Solid.NotFollowed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
